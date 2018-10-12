0f8dc298afe10383309ef6a28ed71ccb5f3ea579
Diego França <diegobmf@gmail.com>
2018-10-12 15:09:14 -0300

    Arrumando a saída do código para ficar de acordo com o enunciado

--- trabalho_2/MIPS_Trabalho2_Romeu_Diego.asm
+++ trabalho_2/MIPS_Trabalho2_Romeu_Diego.asm
@@ -1,13 +1,13 @@
 # 1) Escreva um programa em linguagem de montagem para MIPS usando, preferencialmente, o
-# simulador MARS como plataforma de desenvolvimento e validação. A sua aplicação deverá
-# calcular a o máximo divisor comum (MDC) e o mínimo múltiplo comum (MMC). Seguem
-# os requisitos de implementação:
-# ? Sua aplicação deverá receber em entrada em console dois números inteiros positivos e
-# maiores que 1 e, em seguida, imprimir uma mensagem como resultado da operação.
+# simulador MARS como plataforma de desenvolvimento e validaï¿½ï¿½o. A sua aplicaï¿½ï¿½o deverï¿½
+# calcular a o mï¿½ximo divisor comum (MDC) e o mï¿½nimo mï¿½ltiplo comum (MMC). Seguem
+# os requisitos de implementaï¿½ï¿½o:
+# ? Sua aplicaï¿½ï¿½o deverï¿½ receber em entrada em console dois nï¿½meros inteiros positivos e
+# maiores que 1 e, em seguida, imprimir uma mensagem como resultado da operaï¿½ï¿½o.
 # ? Exemplo de entrada:
 # 9
 # 6
-# ? Saída para o exemplo acima:
+# ? Saï¿½da para o exemplo acima:
 # MDC: 3
 # MMC: 18
 # ? Outros exemplos:
@@ -17,7 +17,7 @@
 # MDC: 1
 # MMC: 60
 # %%%%%%%%
-# ­1
+# ï¿½1
 # 4
 # =======
 # Entrada invalida.
@@ -25,41 +25,35 @@
 	
 ## TRABALHO 02 
 ### Romeu Carvalho Antunes - 15/0147554
-### Diego Barbosa da Mota França - 14/0136355
+### Diego Barbosa da Mota Franï¿½a - 14/0136355
 
 .data
 
-in_string: 			.asciiz "Digite um numero Inteiro: \n"
-string_mmc: 		.asciiz "O Resultado do MMC foi: \n"
-string_mdc: 		.asciiz "O Resultado do MDC foi: \n"
-string_invalido: 	.asciiz "\n %%%%%%%%%% Numero digitado inválido %%%%%%%%%% \n"
-out_string: 		.asciiz "O inteiro digitado foi : \n"
-quebra_linha: 		.asciiz "\n"
+string_separador:	.asciiz "========\n"
+string_mdc: 		.asciiz "MDC: "
+string_mmc: 		.asciiz "\nMMC: "
+string_invalido: 	.asciiz "Entrada invalida\n"
 
 .text
 
 main:
-	li $v0, 4					# Print instrução para o usuário
-	la $a0, in_string			# Carrega a string em a0
-	syscall
-	
-	li $v0,5					# função para ler inteiro
+	li $v0,5					# funï¿½ï¿½o para ler inteiro
 	syscall					
 	move $t0, $v0				# Armazena primeiro numero em t0
-
-	li $v0, 4					# Print instrução para o usuário
-	la $a0, in_string			# Carrega a string em a0
+	
+	li $v0,5					# funï¿½ï¿½o para ler inteiro
+	syscall					
+	move $t1, $v0				# Armazena segundo numero em t1
+	
+	li $v0, 4				# comando para print de string
+	la $a0, string_separador		# carrega em a0 a string para ser printada
 	syscall
 	
-	li $v0,5					# função para ler inteiro
-	syscall					
-	move $t1, $v0				# Armazena primeiro numero em t1
+	bltz $t0, out_of_here		# verifica se valor ï¿½ invï¿½lido caso seja pula para funï¿½ï¿½o out_of_here
+	bltz $t1, out_of_here		# verifica se valor ï¿½ invï¿½lido caso seja pula para funï¿½ï¿½o out_of_here
 	
-	bltz $t0, out_of_here		# verifica se valor é inválido caso seja pula para função out_of_here
-	bltz $t1, out_of_here		# verifica se valor é inválido caso seja pula para função out_of_here
-	
-	slt $t2, $t1, $t0					# Verifica se o segundo numero inserido é menor que o primeiro
-	bne $t2, $zero, arruma_valores		# Caso seja menor a função arruma valor irá arrumar as posições
+	slt $t2, $t1, $t0					# Verifica se o segundo numero inserido ï¿½ menor que o primeiro
+	bne $t2, $zero, arruma_valores		# Caso seja menor a funï¿½ï¿½o arruma valor irï¿½ arrumar as posiï¿½ï¿½es
 	add $s0, $zero, $t0					# Salva valor de entrada em s0 para mmc
 	add $s1, $zero, $t1					# salva valor de entrada em s1 para mmc
 	j calculaMDC
@@ -79,19 +73,19 @@ main:
 calculaMDC:
 
 	bne $t1, 0, loopMDC						# Verifica se t1 != 0
-	j finalMDC							# Se t1 = 0 Resultado do MDC é o prório T0
+	j finalMDC							# Se t1 = 0 Resultado do MDC ï¿½ o prï¿½rio T0
 		
 	loopMDC:
-		div $t0, $t1 						# Divisão de inteiros
+		div $t0, $t1 						# Divisï¿½o de inteiros
 		mflo $t4							# Resultado do cociente
-		mfhi $t2							# Resto da divisão
+		mfhi $t2							# Resto da divisï¿½o
 		move $t0, $t1						# Movendo o valor de t1 para t0
 		move $t1, $t2						# Movendo o resto t2 para t1
-		j calculaMDC						# Retorna a Função de calcular MDC
+		j calculaMDC						# Retorna a Funï¿½ï¿½o de calcular MDC
 
 finalMDC:
 	add $s7, $zero, $t0 	# Armazena o resultado do mdc em S7
-		
+
 	li $v0, 4				# comando para print de string
 	la $a0, string_mdc		# carrega em a0 a string para ser printada
 	syscall
@@ -100,15 +94,10 @@ finalMDC:
 	li $v0, 1				# comando para print de inteiro
 	syscall
 	
-	li $v0, 4				# comando para print string
-	la $a0, quebra_linha	# printa string de quebra de linha que é carregada en a0
-	syscall
-	
 	li $t7, 100 			# garante o primeiro loop do mmc
-	syscall
 	
-	mult $s0,$s1 			# multiplicação para mmc
-	mflo $s2				# Aarmazena o resultado da multiplicação em s2
+	mult $s0,$s1 			# multiplicaï¿½ï¿½o para mmc
+	mflo $s2				# Aarmazena o resultado da multiplicaï¿½ï¿½o em s2
 	move $t0, $s0			# Carrega valores iniciais para calculo do mmc
 	move $t1, $s1			# Carrega valores iniciais para calculo do mmc
 	
@@ -116,15 +105,15 @@ finalMDC:
 	
 calculaMMC:
 	
-	bne $t7, 0, loopMMC						# Condição de loop para calculo do mmc
+	bne $t7, 0, loopMMC						# Condiï¿½ï¿½o de loop para calculo do mmc
 	div $s2, $t0							# divisao dos numeros de entrada 
 	mflo $s3								# resultado da divisao
 	j resultadoMMC
 	
 	loopMMC:
-		div $t0, $t1 						# Divisão de inteiros
+		div $t0, $t1 						# Divisï¿½o de inteiros
 		mflo $t4							# Resultado do cociente
-		mfhi $t7							# Resto da divisão
+		mfhi $t7							# Resto da divisï¿½o
 		move $t0, $t1						# movendo valor de t1 para t0
 		move $t1, $t7						# movendo valor do resto para t1
 		j calculaMMC
@@ -134,7 +123,7 @@ resultadoMMC:
 			
 		
 	li $v0, 4				# comando de print de string
-	la $a0, string_mmc		# carrega em a0 a string que será printada
+	la $a0, string_mmc		# carrega em a0 a string que serï¿½ printada
 	syscall
 	
 	move $a0, $s3			# carrega em a0 o resultado do mmc
@@ -149,7 +138,7 @@ resultadoMMC:
 out_of_here:
 	
 	li $v0, 4							# comando de print de string
-	la $a0, string_invalido				# avisa que o numero entrado é inválido
+	la $a0, string_invalido				# avisa que o numero entrado ï¿½ invï¿½lido
 	syscall
 	
 	li $v0, 10							# comando para terminar o programa
